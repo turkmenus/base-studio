e26bd00fb5b715b894e50decbaba6eb1a837e8a0
turkmenus <turkmenus@users.noreply.github.com>
2026-09-14 23:38:26 +0300

    fix: update branch name from 'main' to 'master' in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
       provider: 'github',
       owner: 'turkmenus',
       repo: 'base-studio',
-      branch: 'main'
+      branch: 'master'
     }
   },
   devtools: { enabled: true },
